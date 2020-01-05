7fca26788884bd428dc72656621bbbe3272d87b5
chapu <chapupupupupu@yahoo.co.jp>
2020-01-05 15:25:20 +0900

    ブラウザにとりあえず表示させるまでのファイルを作成。

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+// ブラウザにエラーを表示させる
+ini_set('display_errors', 1);
+
+// データベースの設定
+define('DSN', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once(__DIR__ . '/config.php');
+require_once(__DIR__ . '/functions.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="container">
+    <h1>Todos</h1>
+    <form action="">
+      <input type="text" id="new_todo" placeholder="What needs to be done?">
+    </form>
+    <ul>
+      <li>
+        <input type="checkbox">
+        <span>Do something</span>
+        <div class="delete_todo">x</div>
+      </li>
+      <li>
+        <input type="checkbox" checked>
+        <span class="done">Do something again!!!</span>
+        <div class="delete_todo">x</div>
+      </li>
+    </ul>
+  </div>
+</body>
+</html>
