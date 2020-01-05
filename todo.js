2e03bbf418aa39f557c86a47d28f6d6ca8f24823
chapu <chapupupupupu@yahoo.co.jp>
2020-01-05 18:52:07 +0900

    Todoの削除機能の実装。

--- todo.js
+++ todo.js
@@ -19,4 +19,40 @@ $(function () {
     })
   });
 
+}); $(function () {
+  'use strict';
+
+  // update
+  $('#todos').on('click', '.update_todo', function () {
+    // idを取得
+    var id = $(this).parents('li').data('id');
+    // ajax処理
+    $.post('_ajax.php', {
+      id: id,
+      mode: 'update',
+      token: $('#token').val()
+    }, function (res) {
+      if (res.state === '1') {
+        $('#todo_' + id).find('.todo_title').addClass('done');
+      } else {
+        $('#todo_' + id).find('.todo_title').removeClass('done');
+      }
+    })
+  });
+
+  // delete
+  $('#todos').on('click', '.delete_todo', function () {
+    // idを取得
+    var id = $(this).parents('li').data('id');
+    // ajax処理
+    if (confirm('are you sure?')) {
+      $.post('_ajax.php', {
+        id: id,
+        mode: 'delete',
+        token: $('#token').val()
+      }, function () {
+        $('#todo_' + id).fadeOut(400);
+      })
+    }
+  });
 });
